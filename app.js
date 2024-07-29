d018c53aa30542798ea41335746d3f3b491e6d63
Gerhard Otto <gerrieotto123@gmail.com>
2024-07-29 22:27:04 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.section--page');
+    hiddenElements.forEach((element) => {
+        observer.observe(element);
+    });
+
+    function copyEmail() {
+        const emailText = document.getElementById("email").innerText.trim();
+        navigator.clipboard.writeText(emailText).then(function() {
+            alert('Email copied to clipboard');
+        }, function(err) {
+            console.error('Could not copy text: ', err);
+        });
+    }
+
+    const emailElement = document.getElementById("email");
+    if (emailElement) {
+        emailElement.addEventListener('click', copyEmail);
+    }
+});
