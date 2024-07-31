12b8d7c1ef0e534eaef248fc44aac4a2335ae0d0
Gerhard Otto <gerrieotto123@gmail.com>
2024-07-31 22:06:53 +0200

    add nav bar (broken)

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+function toggleMenu(){
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
@@ -27,4 +34,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (emailElement) {
         emailElement.addEventListener('click', copyEmail);
     }
+
 });
